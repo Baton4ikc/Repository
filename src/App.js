aa69eff6fee25f50a535a5a2a724f81a6d0fe64d
Baton4ikc <sasha4kaaa@gmail.com>
2022-07-11 19:50:17 +1000

    project

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <body>    
+      <div className="app">
+         <header class="head">
+            <div class="wh-border">
+              <div class="content-br">
+                мы сочетаем дизайн, мышление и цифровое ремесло
+                <h2>Создание Цифрового Опыта</h2>
+                <button>учить больше</button>
+              </div>
+            </div>
+        </header>
+        <main class="main">
+          <div class="block_1">
+            <div class="sp-content">|
+              <div class="left">
+                <h2>egrtgrtgregre</h2>
+                Cуществует два основных способа предварительного форматирования: при помощи 
+                непосредственного оформления текста в текстовом редакторе, которое должно будет/
+                сохраниться при помещении в верстку предполагается использование текстовых форматов,
+              </div>
+              <div class="right">
+                <h2>rtgrgregerg</h2>
+                Cуществует два основных способа предварительного форматирования: при помощи 
+                непосредственного оформления текста в текстовом редакторе, которое должно будет 
+                сохраниться при помещении в верстку предполагается использование текстовых форматов,
+              </div>
+            </div>
+          </div>
+          <div class="block_2"> 
+            <div className="bt-content">rthrthrthrthrthrthrthrthrt</div>
+            <div class="position"></div>
+          </div>
+        </main>
+      </div>
+    </body>    
+  );
+}
+
+export default App;
